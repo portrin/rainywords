f6277604d7dba341e652d9e2248c86eee73377bb
Trin Sengchareon <melonn.qq@gmail.com>
2019-10-19 12:55:11 +0700

    share create WordLibrary

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import pygame
 from os import path
-from word import Word
+from word import Word, WordLibrary
 
 width = 1200
 height = 800
+BACKSPACE = 8
+RETURN = 13
 
 # test words
 words = ["hello", "int", "hi", "good", "read", "apple", "thanks", "tree", "sun", "moon"]
@@ -18,11 +20,15 @@ title = pygame.image.load("src/rainywords.png").convert_alpha()
 class Player():
     def __init__(self, words, score):
         #track current words on screen.
-        self.current_word_list = [] 
+        self.current_word_as_string_list = []
+        self.current_word_list = []
         #words list that can be generated.
-        self.words_library = Word(words)
+        self.words_library = WordLibrary(words)
         #track users scores
         self.score = 0
+        #current word that typing
+        self.pressed_word = ""
+
 
 
 
@@ -30,7 +36,7 @@ class Player():
 
 
 def redrawWindow(win):
-    win.fill((255,230,230))
+    win.fill((255,255,255))
     pygame.display.update()
 
 def menu():
@@ -41,18 +47,35 @@ def menu():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RETURN]:
             return
-        win.fill((255,230,230))
+        win.fill((255,255,255))
         win.blit(title, (350,200))
         pygame.display.update()
 
 def main():
     run = True
     menu()
+    player = Player(words, score=0)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                #player press return to submit word
+                if event.key == RETURN:
+                    #if what player types match
+                    if player.pressed_word in [for a in player.current_word_list]:
+                        #clear correct word from the screen
+                        player.current_word_list[player.current_word_list.index(player.pressed_word)].clear_words()
+                        #remove correct word from the list
+                        player.current_word_list.remove(player.pressed_word)
+                        player.score += player.pressed_word.length()
+                        print(player.score)
+                        
+
+                player.pressed_word += pygame.key.name(event.key)
+                print(player.pressed_word)
+            
         redrawWindow(win)
 
 main()
--- word.py
+++ word.py
@@ -1,13 +1,29 @@
 import random
 
 class Word():
-    def __init__(self, words):
+    def __init__(self, word, win_width, win_height):
         # collection of random words from server to client
-        self.words = words
+        self.word = word
+        self.x = random.randint(100, win_width - 100)
+        self.y = 0
+        self.vel = 0.005
+    
+    def clear_words(self):
+        pass
+
+    def string_compare(self, str="", word):
+        return str == self.word
+
+    def word_to_string(self):
+        return self.word
+
+class WordLibrary():
+    def __init__(self, words):
+        self.word_library = words # words come from .csv file that was loaded in client.py
 
     def generate_words(self):
         try:
-            word = random.choice(self.words)
+            word = Word(random.choice(self.word_library))
         except random.error as error:
             print(str(error))
         return word
