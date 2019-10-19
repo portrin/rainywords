048b0c65d6c1ed48b74e68fd3e4999d46a83ac40
Trin Sengchareon <melonn.qq@gmail.com>
2019-10-19 10:21:42 +0700

    add title and src

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import pygame
+from os import path
 from word import Word
 
 width = 1200
@@ -6,17 +7,47 @@ height = 800
 
 # test words
 words = ["hello", "int", "hi", "good", "read", "apple", "thanks", "tree", "sun", "moon"]
-#word = Word(words)
+
+font = pygame.font.get_default_font()
 
 win = pygame.display.set_mode((width, height))
+
 pygame.display.set_caption("Rainy Words")
+title = pygame.image.load("src/rainywords.png").convert_alpha()
+
+class Player():
+    def __init__(self, words, score):
+        #track current words on screen.
+        self.current_word_list = [] 
+        #words list that can be generated.
+        self.words_library = Word(words)
+        #track users scores
+        self.score = 0
+
+
+
+
+
 
 def redrawWindow(win):
     win.fill((255,230,230))
     pygame.display.update()
 
+def menu():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            return
+        win.fill((255,230,230))
+        win.blit(title, (350,200))
+        pygame.display.update()
+
 def main():
     run = True
+    menu()
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- word.py
+++ word.py
@@ -5,8 +5,9 @@ class Word():
         # collection of random words from server to client
         self.words = words
 
-    def generate_words():
+    def generate_words(self):
         try:
             word = random.choice(self.words)
         except random.error as error:
             print(str(error))
+        return word
