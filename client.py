ed0848603b2145333c952fd69e3c5d47901cbc0c
Trin Sengchareon <melonn.qq@gmail.com>
2019-10-24 16:14:13 +0700

    add timer, but waiting for play again method

--- client.py
+++ client.py
@@ -3,16 +3,22 @@ from os import path
 from word import Word, WordLibrary, CurrentWordList
 
 global DELAY
+
 width = 1200
 height = 800
 vel = 0.02
+minute = 0
+second = 0
+minute_text = '0'
+second_text = '00'
 BACKSPACE = 8
 RETURN = 13
 SPACE = 32
-TIMER = 20
+COUNTDOWN = 10 # countdown per game 180 seconds
+CLOCKTICK_EVENT = 20
 TIMER_GEN_WORD = 21
 MISSED_WORD = 22
-DELAY = 2300
+DELAY = 2200
 player_id = 0
 pygame.font.init()
 pygame.mixer.init(44100, -16,2,2048)
@@ -48,10 +54,10 @@ class Player():
 
 
 
-def redrawWindow(win, player, vel, DELAY):
+def redrawWindow(win, player, vel, DELAY, time_text):
     win.fill((255,255,255))
     win.blit(gameImage, (0,0))
-    print(player.username)
+    win.blit(time_text, (555,35))
     username = font.render(player.username, True, (0,0,0))
     win.blit(username, (32,29)) # render username on screen
     user_text = font.render(player.pressed_word, True, (0,0,0)) # render interactive typing
@@ -86,7 +92,7 @@ def frontpage(): #enter name page
     name = ""
     # pygame.mixer.music.load("src/bgm.ogg")
     pygame.mixer.Channel(0).set_volume(0.7)
-    pygame.mixer.Channel(0).play(pygame.mixer.Sound("src/bgm.ogg"))
+    pygame.mixer.Channel(0).play(pygame.mixer.Sound("src/bgm.ogg"), -1)
     while True:
         text = font.render(name, True, (0,0,0))
         win.blit(frontpageImage, (0,0))
@@ -108,16 +114,18 @@ def frontpage(): #enter name page
 def main():
     run = True
     name = frontpage()
-    print(pygame.event.get())
     welcome(name)
 
     #GAME SETUP
+    time_text = font.render('3 : 00', True, (0,0,0))
+    timeleft = COUNTDOWN # set variable to track timeleft
     clock = pygame.time.Clock()
     player = Player(words, 0, name, player_id)
     #words list that can be generated.
     words_library = WordLibrary(words, vel)
     #timer for generate word
     pygame.time.set_timer(TIMER_GEN_WORD, DELAY)
+    pygame.time.set_timer(CLOCKTICK_EVENT, 1000) # clock ticks every 1 second.
 
     while run:
         #clock.tick(60)
@@ -125,6 +133,23 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+            # clock tick event (timer)
+            elif event.type == CLOCKTICK_EVENT:
+                # do sth
+                timeleft -= 1
+                if timeleft == 0:
+                    #go to play again screen
+                    print('game over')
+                    pass
+                minute = timeleft // 60
+                second = timeleft - (minute * 60)
+                minute_text = str(minute)
+                if second < 10:
+                    second_text = '0' + str(second)
+                else:
+                    second_text = str(second)
+                time_text = font.render(minute_text + ' : ' + second_text, True, (0,0,0)) # timer update (render)
+                pass
             #word genarating event
             elif event.type == TIMER_GEN_WORD:
                 player.current_word_list.current_word_list.append(words_library.generate_words())
@@ -168,6 +193,6 @@ def main():
                 else:
                     player.score -= len(event.word)
                 print(player.score)
-        redrawWindow(win, player, vel, DELAY)
+        redrawWindow(win, player, vel, DELAY, time_text)
 
 main()
